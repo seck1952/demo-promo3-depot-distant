0cfd3896ba3b24eacb3000e3c3b3c9fcf11fa770
Elhadji Seck <seckmoustapha91@gmail.com>
2018-05-17 17:19:26 +0000

    changement du fichier formateur

--- Formateur.java
+++ Formateur.java
@@ -6,4 +6,18 @@ private String adress;
 public Formateur(){
 super();
 }
+
+public Formateur(){
+
+}
+public String getNom(){
+return this.nom
+
+}
+public String getPrenom(){
+return this.prenom
+}
+public String getAdress(){
+return this.Adress
+}
 }
